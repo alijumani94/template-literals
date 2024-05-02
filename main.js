bce99dfc4f50f8c783dfd0fc46ac9184addeb2b3
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2024-05-02 08:04:01 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var model = "civic rebirth";
+var engine = "1.8cc";
+console.log("-My car is ".concat(model, ". \n-its engine capacity is ").concat(engine, ".")); //applying features of template literals
+//print values of variables in string
+//started next line in same string text 
---
+++ main.ts
@@ -0,0 +1,6 @@
+let model: string= "civic rebirth";
+let engine: string= "1.8cc";
+console.log(`-My car is ${model}. 
+-its engine capacity is ${engine}.`);  //applying features of template literals
+                                       //print values of variables in string
+                                       //started next line in same string text 
